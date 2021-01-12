9cd96028906045dee764c9812e25afa4af5b2209
Maria <marybelousova@yahoo.com>
2021-01-11 22:43:43 -0500

    fetching third party API

---
+++ script.js
@@ -0,0 +1,14 @@
+const count = 10;
+const apiKey = '0LEYwFpdvXCuBvZyUSN7DoyGuVf4imgLFyiUkroKeTU';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&${count}`;
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data)
+    } catch (error) {
+        
+    }
+}
+getPhotos();
