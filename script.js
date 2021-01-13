d55bd239ae15c9dc21351b826162916a9c59afed
Maria <marybelousova@yahoo.com>
2021-01-13 18:52:54 -0500

    fetching data from API

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
 const count = 10;
 const apiKey = '0LEYwFpdvXCuBvZyUSN7DoyGuVf4imgLFyiUkroKeTU';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&${count}`;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function setAttributes(element, attributes) {
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+}
+
+function displayPhotos() {
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+        const img = document.createElement('img');
+        // img.setAttribute('src', photo.urls.regular);
+        // img.setAttribute('atl', photo.alt_description);
+        // img.setAttribute('title', photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
 
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data)
+        photosArray = await response.json();
+        displayPhotos();
     } catch (error) {
         
     }
