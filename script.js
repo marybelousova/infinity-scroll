e589e5b170b5c2c0fcf9c6f577a9b319c83e3473
Maria <marybelousova@yahoo.com>
2021-01-13 19:29:44 -0500

    fix bug and clean code

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ function imageLoaded() {
     if(imagesLoaded === totalImages) {
         ready = true;
         loader.hidden = true; 
-        console.log('ready = ', totalImages)
     }
 }
 
@@ -32,16 +31,11 @@ function displayPhotos() {
     console.log('total images', totalImages)
     photosArray.forEach((photo) => {
         const item = document.createElement('a');
-        // item.setAttribute('href', photo.links.html);
-        // item.setAttribute('target', '_blank');
         setAttributes(item, {
             href: photo.links.html,
             target: '_blank',
         });
         const img = document.createElement('img');
-        // img.setAttribute('src', photo.urls.regular);
-        // img.setAttribute('atl', photo.alt_description);
-        // img.setAttribute('title', photo.alt_description);
         setAttributes(img, {
             src: photo.urls.regular,
             alt: photo.alt_description,
